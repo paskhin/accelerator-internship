815c7949eb1cf44c7bd7258ffa9f4f6134e0dabc
Ignat Paskhin <paskhin_92@mail.ru>
2024-01-31 23:55:41 +0300

    исправляет планшет

--- source/js/hero-swiper.js
+++ source/js/hero-swiper.js
@@ -3,13 +3,12 @@ import { Navigation, Pagination } from 'swiper/modules';
 const paginationBlock = document.querySelector('[data-hero="hero-pagination"]');
 const mediaTab = window.matchMedia('(min-width: 768px)');
 const mediaMob = window.matchMedia('(min-width: 320px)');
-
 export const initHeroSwiper = () => {
   const slides = document.querySelectorAll('[data-hero="hero-item"]');
   const slidesArr = [...slides];
   const swiper = new Swiper('[data-hero="hero-slider"]', {
-    modules: [Navigation, Pagination],
     loop: true,
+    modules: [Pagination],
     pagination: {
       el: '[data-hero="hero-pagination"]',
       clickable: true,
@@ -31,13 +30,14 @@ export const initHeroSwiper = () => {
     const textesActive = document.querySelectorAll('[data-hero="content"]');
     textesActive.forEach((item, index) => {
       if (mediaTab.matches && index === slideActive) {
-        const height = item.clientHeight + 60;
-        paginationBlock.style.bottom = `${height}px`;
+        const heightTab = item.clientHeight + 60;
+        paginationBlock.style.bottom = `${heightTab}px`;
       } else if (mediaMob.matches && index === slideActive){
         const height = item.clientHeight + 20;
         paginationBlock.style.bottom = `${height}px`;
       }
-    });
+    }
+    );
   });
 
 };
